1f7120ede2ae97ebd79f8d5dc6a41a61314130a3
Dat Dang <dangtuandat1995@gmail.com>
2023-04-26 08:09:12 +0700

    move appwrite init to provider

--- lib/app.dart
+++ lib/app.dart
@@ -1,26 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:school_me/application/app_router/provider/app_router_provider.dart';
-import 'package:school_me/application/appwrite/provider/appwrite_provider.dart';
 
-class MyApp extends HookConsumerWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final router = ref.read(appRouterProvider);
-
-    useEffect(() {
-      final client = ref.read(appwriteClientProvider);
-      client
-              .setEndpoint('https://localhost/v1') // Your Appwrite Endpoint
-              .setProject('644732c2d0c36b537e5a') // Your project ID
-              .setSelfSigned() // Use only on dev mode with a self-signed SSL cert
-          ;
-      return null;
-    }, []);
 
     return MaterialApp.router(
       title: 'School Me',
--- lib/application/appwrite/provider/appwrite_provider.dart
+++ lib/application/appwrite/provider/appwrite_provider.dart
@@ -5,5 +5,11 @@ part 'appwrite_provider.g.dart';
 
 @riverpod
 Client appwriteClient(AppwriteClientRef ref) {
-  return Client();
+  final client = Client();
+  client
+          .setEndpoint('https://localhost/v1') // Your Appwrite Endpoint
+          .setProject('644732c2d0c36b537e5a') // Your project ID
+          .setSelfSigned() // Use only on dev mode with a self-signed SSL cert
+      ;
+  return client;
 }
