d65aa23dc9372c9189390ebb4491832dc81a4889
Dat Dang <dangtuandat1995@gmail.com>
2023-04-27 08:22:46 +0700

    init auth page

--- lib/application/app_router/router/app_router.dart
+++ lib/application/app_router/router/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 
+import '../../../presentation/pages/auth_page.dart';
 import '../../../presentation/pages/home_page.dart';
 import '../../../presentation/pages/schools_page.dart';
 import '../../../presentation/pages/splash_page.dart';
@@ -10,7 +11,8 @@ part 'app_router.gr.dart';
 class AppRouter extends _$AppRouter {
   @override
   List<AutoRoute> get routes => [
-        AutoRoute(page: SplashRoute.page, initial: true),
+        AutoRoute(page: SplashRoute.page),
+        AutoRoute(page: AuthRoute.page, initial: true),
         AutoRoute(page: HomeRoute.page),
         AutoRoute(page: SchoolsRoute.page),
       ];
